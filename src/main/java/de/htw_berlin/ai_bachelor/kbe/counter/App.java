47ce8da43284302d9b7c193e65ee150b69e09007
Yoggi Firmanda <y.firmanda@netpoint-media.de>
2015-05-22 11:12:58 +0200

    JUnit Test added and works
    Output CounterTriple by App exec
    JUnit Test for CounterSimple

--- src/main/java/de/htw_berlin/ai_bachelor/kbe/counter/App.java
+++ src/main/java/de/htw_berlin/ai_bachelor/kbe/counter/App.java
@@ -10,11 +10,20 @@ public class App
         c.increment();
         
         System.out.println("CounterSimple = " +c.getCounter());
+        //Ausgabe: "CounterSimple = 2"
         
         Counter c2 = CounterFactory.getCounterInstance("double");
         c2.increment();
         c2.increment();
         
         System.out.println("CounterDouble = " +c2.getCounter());
+        //Ausgabe: "CounterDouble = 4"
+        
+        Counter c3 = CounterFactory.getCounterInstance("triple");
+        c3.increment();
+        c3.increment();
+        
+        System.out.println("CounterTriple = " +c3.getCounter());
+        //Ausgabe: "CounterDouble = 6"
     }
 }
