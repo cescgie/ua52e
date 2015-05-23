3a517f8a43605eaea1781d06c0ea0b121676dcbb
Yoggi Firmanda <yoggifirmanda@gmail.com>
2015-05-23 19:50:54 +0200

    Update App.java

--- src/main/java/de/htw_berlin/ai_bachelor/kbe/counter/App.java
+++ src/main/java/de/htw_berlin/ai_bachelor/kbe/counter/App.java
@@ -15,15 +15,15 @@ public class App
         //Ausgabe: "CounterSimple = 2"
         
         Counter c2 = CounterFactory.getCounterInstance("double");
-        c2.increment(); //2+2=2
-        c2.increment(); //4+2=6
+        c2.increment(); //0+2=2
+        c2.increment(); //2+2=4
         
         System.out.println("CounterDouble = " +c2.getCounter());
         //Ausgabe: "CounterDouble = 4"
         
         Counter c3 = CounterFactory.getCounterInstance("triple");
+        c3.increment(); //0+3=3
         c3.increment(); //3+3=6
-        c3.increment(); //6+3=9
         
         System.out.println("CounterTriple = " +c3.getCounter());
         //Ausgabe: "CounterDouble = 6"
